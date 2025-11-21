660bf31904c415c7257fa013dc1e567b9070dd53
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2025-11-20 22:54:41 -0600

    Refactor Mandelbrot generator for clarity and efficiency

--- mandelbrot-generator.cu
+++ mandelbrot-generator.cu
@@ -12,74 +12,75 @@
 #define CHANNELS 3
 #define NUM_BLOCKS (int)ceil(((double)IMG_W * IMG_H) / NUM_THREADS)
 
-// added changes
-
 typedef struct complexNumber {
     double real;
     double imag;
 } C;
 
-__device__ void complexAdd(C *z, C *cnst, C *res) {
-    res->real = z->real + cnst->real;
-    res->imag = z->imag + cnst->imag;
+__device__ void addComplex(C *a, C *b, C *out) {
+    out->real = a->real + b->real;
+    out->imag = a->imag + b->imag;
 }
 
-__device__ void complexMultiply(C *x, C *y, C *res) {
-    res->real = (x->real * y->real) - (x->imag * y->imag);
-    res->imag = (x->real * y->imag) + (x->imag * y->real);
+__device__ void mulComplex(C *a, C *b, C *out) {
+    out->real = (a->real * b->real) - (a->imag * b->imag);
+    out->imag = (a->real * b->imag) + (a->imag * b->real);
 }
 
-__device__ double complexAbsolute(C *c) {
-    return sqrt((c->real * c->real) + (c->imag * c->imag));
+__device__ double absComplex(C *z) {
+    return sqrt(z->real * z->real + z->imag * z->imag);
 }
 
-__device__ float mandelbrot(C *cnst) {
+__device__ float computeEscapeTime(C *c) {
     C z = {0.0, 0.0};
-    C zSq;
+    C z2;
     int i;
 
-    for(i = 0; i < MAX_ITR; i++) {
-        if(z.real * z.real + z.imag*z.imag > 4.0) {
-            break;
-        }
+    for (i = 0; i < MAX_ITR; i++) {
+        if (z.real * z.real + z.imag * z.imag > 4.0) break;
 
-        complexMultiply(&z, &z, &zSq);
-        complexAdd(&zSq, cnst, &z);
+        mulComplex(&z, &z, &z2);
+        addComplex(&z2, c, &z);
     }
 
-    if(i == MAX_ITR) {
-        return MAX_ITR;
-    }
-    
-    double mag = sqrt(z.real * z.real + z.imag * z.imag);
+    if (i == MAX_ITR) return MAX_ITR;
+
+    double mag = absComplex(&z);
     float smooth = i + 1 - log2f(log2f(mag));
     return smooth;
 }
 
-__device__ void getColor(float smooth, unsigned char *r, unsigned char *g, unsigned char *b)
-{
+__device__ void mapColor(float smooth, unsigned char *r, unsigned char *g, unsigned char *b) {
     float t = smooth / (float)MAX_ITR;
-    *r = (unsigned char)(9*(1-t)*t*t*t*255);
-    *g = (unsigned char)(15*(1-t)*(1-t)*t*t*255);
-    *b = (unsigned char)(8.5*(1-t)*(1-t)*(1-t)*t*255);
+    *r = (unsigned char)(9  * (1-t) * t*t*t * 255);
+    *g = (unsigned char)(15 * (1-t) * (1-t) * t*t * 255);
+    *b = (unsigned char)(8.5*(1-t) * (1-t) * (1-t) * t * 255);
 }
 
-
-__global__ void parallelMandelbrot(unsigned char *dev_image, double REAL_MIN, double IMAG_MIN, double INC_REAL, double INC_IMAG) {
+__global__ void mandelbrotKernel(
+    unsigned char *img,
+    double REAL_MIN, double IMAG_MIN,
+    double dReal, double dImag
+) {
     int x = threadIdx.x + blockIdx.x * blockDim.x;
     int y = threadIdx.y + blockIdx.y * blockDim.y;
 
-    if(x < IMG_W && y < IMG_H) {
-        double real = REAL_MIN + ((double)x * INC_REAL);
-        double imag = IMAG_MIN + ((double)y * INC_IMAG);
-        C pcNum = {real, imag};
-        float smooth = mandelbrot(&pcNum);
-        int pixelIdx = (y * IMG_W + x) * CHANNELS;
+    if (x < IMG_W && y < IMG_H) {
+        double real = REAL_MIN + x * dReal;
+        double imag = IMAG_MIN + y * dImag;
+
+        C point = {real, imag};
+
+        float val = computeEscapeTime(&point);
+
+        int idx = (y * IMG_W + x) * CHANNELS;
+
         unsigned char r, g, b;
-        getColor(smooth, &r, &g, &b);
-        dev_image[pixelIdx + 0] = r;    
-        dev_image[pixelIdx + 1] = g;
-        dev_image[pixelIdx + 2] = b;
+        mapColor(val, &r, &g, &b);
+
+        img[idx    ] = r;
+        img[idx + 1] = g;
+        img[idx + 2] = b;
     }
 }
 
@@ -89,32 +90,32 @@ int main() {
     double IMAG_MIN = -0.85;
     double IMAG_MAX = 0.8375;
 
-    double INC_REAL = (REAL_MAX - REAL_MIN) / IMG_W;
-    double INC_IMAG = (IMAG_MAX - IMAG_MIN) / IMG_H;
+    double dReal = (REAL_MAX - REAL_MIN) / IMG_W;
+    double dImag = (IMAG_MAX - IMAG_MIN) / IMG_H;
 
-    size_t img_bytes = IMG_W * IMG_H * CHANNELS * sizeof(unsigned char);
-    unsigned char *host_image = (unsigned char *)malloc(img_bytes);
-    if(host_image == NULL) {
-        fprintf(stderr, "FAILED TO ALLOCATE MEMORY\n");
-        return 1;
-    }
+    size_t img_bytes = IMG_W * IMG_H * CHANNELS;
+    unsigned char *host_img = (unsigned char *)malloc(img_bytes);
 
-    unsigned char *dev_image;
-    cudaMalloc(&dev_image, img_bytes);
-    dim3 block_dim(32, 32, 1);
-    dim3 grid_dim(ceil((float)IMG_W / 32), ceil((float)IMG_H / 32), 1);
-    clock_t start_time = clock();
-    parallelMandelbrot<<<grid_dim, block_dim>>>(dev_image, REAL_MIN, IMAG_MIN, INC_REAL, INC_IMAG);
+    unsigned char *dev_img;
+    cudaMalloc(&dev_img, img_bytes);
+
+    dim3 blockDim(32, 32);
+    dim3 gridDim(ceil((float)IMG_W / 32), ceil((float)IMG_H / 32));
+
+    clock_t start = clock();
+    mandelbrotKernel<<<gridDim, blockDim>>>(dev_img, REAL_MIN, IMAG_MIN, dReal, dImag);
     cudaDeviceSynchronize();
-    clock_t end_time = clock();
-    double time_taken = (double)(end_time - start_time) / CLOCKS_PER_SEC;
-    printf("Time taken: %f seconds\n", time_taken);
-    cudaMemcpy(host_image, dev_image, IMG_W * IMG_H * CHANNELS * sizeof(unsigned char), cudaMemcpyDeviceToHost);
-    if(!stbi_write_png("mandelbrot_set.png", IMG_W, IMG_H, CHANNELS, host_image, IMG_W * CHANNELS)) {
+    clock_t end = clock();
+
+    printf("Time taken: %f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+
+    cudaMemcpy(host_img, dev_img, img_bytes, cudaMemcpyDeviceToHost);
+
+    if (!stbi_write_png("mandelbrot_set.png", IMG_W, IMG_H, CHANNELS, host_img, IMG_W * CHANNELS)) {
         fprintf(stderr, "Failed to write PNG\n");
-        return 1;
     }
 
-    free(host_image);
-    cudaFree(dev_image);
+    free(host_img);
+    cudaFree(dev_img);
+    return 0;
 }
